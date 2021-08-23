b2c90d32cb5c01d2dec5cdf2b231e91dd0c5d22d
LDAP\nthung12 <nthung12@cmc.com.vn>
2021-08-23 11:33:44 +0700

    update fix bug null

--- JavaMongoDBClient/src/main/java/com/khoahung/cmc/application/CreateAssetsOpenKM.java
+++ JavaMongoDBClient/src/main/java/com/khoahung/cmc/application/CreateAssetsOpenKM.java
@@ -70,56 +70,56 @@ public class CreateAssetsOpenKM extends Thread{
 			p5.setName("user_name");
 			p5.setType("String");
 			p5.setMultiple(false);
-			p5.setValues(Arrays.asList(open.getUser_name()));
+			p5.setValues(Arrays.asList(open.getUser_name()==null?"":open.getUser_name()));
 			listP.add(p5);
 			
 			com.khoahung.cmc.entity.Properties p6 = new com.khoahung.cmc.entity.Properties();
 			p6.setName("real_name");
 			p6.setType("String");
 			p6.setMultiple(false);
-			p6.setValues(Arrays.asList(open.getReal_name()));
+			p6.setValues(Arrays.asList(open.getReal_name()==null?"":open.getReal_name()));
 			listP.add(p6);
 			
 			com.khoahung.cmc.entity.Properties p7 = new com.khoahung.cmc.entity.Properties();
 			p7.setName("first_name");
 			p7.setType("String");
 			p7.setMultiple(false);
-			p7.setValues(Arrays.asList(open.getFirst_name()));
+			p7.setValues(Arrays.asList(open.getFirst_name()==null?"":open.getFirst_name()));
 			listP.add(p7);
 			
 			com.khoahung.cmc.entity.Properties p8 = new com.khoahung.cmc.entity.Properties();
 			p8.setName("middle_name");
 			p8.setType("String");
 			p8.setMultiple(false);
-			p8.setValues(Arrays.asList(open.getMiddle_name()));
+			p8.setValues(Arrays.asList(open.getMiddle_name()==null?"":open.getMiddle_name()));
 			listP.add(p8);
 			
 			com.khoahung.cmc.entity.Properties p9 = new com.khoahung.cmc.entity.Properties();
 			p9.setName("last_name");
 			p9.setType("String");
 			p9.setMultiple(false);
-			p9.setValues(Arrays.asList(open.getLast_name()));
+			p9.setValues(Arrays.asList(open.getLast_name()==null?"":open.getLast_name()));
 			listP.add(p9);
 			
 			com.khoahung.cmc.entity.Properties p10 = new com.khoahung.cmc.entity.Properties();
 			p10.setName("mon_name");
 			p10.setType("String");
 			p10.setMultiple(false);
-			p10.setValues(Arrays.asList(open.getMon_name()));
+			p10.setValues(Arrays.asList(open.getMon_name()==null?"":open.getMon_name()));
 			listP.add(p10);
 			
 			com.khoahung.cmc.entity.Properties p11 = new com.khoahung.cmc.entity.Properties();
 			p11.setName("mobile");
 			p11.setType("String");
 			p11.setMultiple(false);
-			p11.setValues(Arrays.asList(open.getMobile()));
+			p11.setValues(Arrays.asList(open.getMobile()==null?"":open.getMobile()));
 			listP.add(p11);
 			
 			com.khoahung.cmc.entity.Properties p12 = new com.khoahung.cmc.entity.Properties();
 			p12.setName("email");
 			p12.setType("String");
 			p12.setMultiple(false);
-			p12.setValues(Arrays.asList(open.getEmail()));
+			p12.setValues(Arrays.asList(open.getEmail()==null?"":open.getEmail()));
 			listP.add(p12);
 			
 			dp.setNodeProperties(listP);
